7b2985db7b7a82565af619064532c5a261085e0f
Matt Mills <sunrisetimes@gmail.com>
2012-03-06 13:59:30 -0600

    index now has stuff in it (with fixes)

---
+++ sinatra_local.rb
@@ -0,0 +1,18 @@
+require 'sinatra/base'
+require 'sinatra'
+require 'haml'
+
+root="/Users/engineyard/desktop"
+get '/' do
+  images = Dir.entries(root).map{|thing| thing unless thing.match(/^\./) || File.directory?("#{root}/#{thing}")}.compact!
+  images = images.map {|name| name if name.match(/(^.+\.jpg|^.+\.png)/)}.compact.sort
+  haml :root_page, :locals => {:images => images, :dir => "", :array_string => ""}
+end
+
+get '/:dir' do
+  dir = params[:dir]
+  images = Dir.entries("#{root}/#{dir}").map{|thing| thing unless thing.match(/^\./) || File.directory?("#{root}/#{dir}/#{thing}")}.compact!
+  images = images.map {|name| name if name.match(/(^.+\.jpg|^.+\.png)/)}.compact.sort
+  array_string="['#{images.join('\',\'')}']"
+  haml :layout_1, :locals => {:images => images, :dir => dir, :array_string => array_string} 
+end
