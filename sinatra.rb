47379a84d54e5e33849d84988ad2ef40a023bdfe
Matt Mills <sunrisetimes@gmail.com>
2012-03-06 11:44:50 -0600

    fix mistake in app code

--- sinatra.rb
+++ sinatra.rb
@@ -1,6 +1,6 @@
 require 'sinatra'
 require 'haml'
-class Gallery << Sinatra::Base
+class Gallery < Sinatra::Base
   root="/home/matt/public_html/images"
   get '/' do
     dir_list = Dir.entries("/home/matt/public_html/images").map{|thing| thing unless thing.match(/^\./) || File.directory?("#{root}/#{dir}/#{thing}")}.compact!
