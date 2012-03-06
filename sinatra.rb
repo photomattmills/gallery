f44c5cca60323c682869db096327e597eed1f055
Matt Mills <sunrisetimes@gmail.com>
2012-03-06 11:18:56 -0600

    add the stuff I need to deploy

--- sinatra.rb
+++ sinatra.rb
@@ -1,13 +1,15 @@
 require 'sinatra'
 require 'haml'
-root="/users/matt/Desktop"
-get '/' do
-  dir_list = Dir.entries("/home/matt/public_html/images")
-  haml :index, :format => :html5, :locals => {:images => dir_list.map{|thing| return thing unless thing.directory?}}
-end
-
-get '/:dir' do
-  dir=params[:dir]
-  images= Dir.entries("#{root}/#{dir}").map{|thing| thing unless thing.match(/^\./) || File.directory?("#{root}/#{dir}/#{thing}")}.compact!
-  haml :layout, :locals => {:images => images, :dir => dir} 
+class Gallery << Sinatra::Base
+  root="/home/matt/public_html/images"
+  get '/' do
+    dir_list = Dir.entries("/home/matt/public_html/images").map{|thing| thing unless thing.match(/^\./) || File.directory?("#{root}/#{dir}/#{thing}")}.compact!
+    haml :index, :format => :html5, :locals => {:images => dir_list}
+  end
+  
+  get '/:dir' do
+    dir=params[:dir]
+    images= Dir.entries("#{root}/#{dir}").map{|thing| thing unless thing.match(/^\./) || File.directory?("#{root}/#{dir}/#{thing}")}.compact!
+    haml :layout, :locals => {:images => images, :dir => dir} 
+  end
 end
