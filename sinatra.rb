81d976fb9bb37bb800e81dd37276b484a2ddebeb
Matt Mills <sunrisetimes@gmail.com>
2012-03-06 12:55:43 -0600

    index now has stuff in it

--- sinatra.rb
+++ sinatra.rb
@@ -4,14 +4,15 @@ require 'haml'
 class Gallery < Sinatra::Base
   root="/home/matt/public_html/images"
   get '/' do
-    dir_list = Dir.entries("/home/matt/public_html/images").map{|thing| thing unless thing.match(/^\./) || File.directory?("#{root}/#{dir}/#{thing}")}.compact!
-    haml :index, :format => :html5, :locals => {:images => dir_list}
+    images = Dir.entries(root).map{|thing| thing unless thing.match(/^\./) || File.directory?("#{root}/#{dir}/#{thing}")}.compact!
+    images = images.map {|name| name if name.match(/(^.+\.jpg|^.+\.png)/)}.compact.sort
+    haml :index, :format => :html5, :locals => {:images => images, :dir = ""}
   end
   
   get '/:dir' do
-    dir=params[:dir]
-    images= Dir.entries("#{root}/#{dir}").map{|thing| thing unless thing.match(/^\./) || File.directory?("#{root}/#{dir}/#{thing}")}.compact!
-    images= images.map {|name| name if name.match(/(^.+\.jpg|^.+\.png)/)}.compact.sort
+    dir = params[:dir]
+    images = Dir.entries("#{root}/#{dir}").map{|thing| thing unless thing.match(/^\./) || File.directory?("#{root}/#{dir}/#{thing}")}.compact!
+    images = images.map {|name| name if name.match(/(^.+\.jpg|^.+\.png)/)}.compact.sort
     array_string="['#{images.join('\',\'')}']"
     haml :layout, :locals => {:images => images, :dir => dir, :array_string => array_string} 
   end
