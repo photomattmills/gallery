c782d182d5064a3b51fda1ef7c1bb072bf02d39e
Matt Mills <sunrisetimes@gmail.com>
2012-03-06 21:41:31 -0600

    file reorganization

--- sinatra.rb
+++ sinatra.rb
@@ -4,16 +4,16 @@ require 'haml'
 class Gallery < Sinatra::Base
   root="/home/matt/public_html/images"
   get '/' do
-    images = Dir.entries(root).map{|thing| thing unless thing.match(/^\./) || File.directory?("#{root}/#{thing}")}.compact!
+    images = Dir.entries(root).map{|thing| thing unless thing.match(/^\./) || File.directory?("#{root}/#{thing}")}.compact! rescue []
     images = images.map {|name| name if name.match(/(^.+\.jpg|^.+\.png)/)}.compact.sort
-    haml :root_page, :locals => {:images => images, :dir => "", :array_string => ""}
+    haml :index, :locals => {:images => images, :dir => "", :array_string => ""}
   end
   
   get '/:dir' do
     dir = params[:dir]
-    images = Dir.entries("#{root}/#{dir}").map{|thing| thing unless thing.match(/^\./) || File.directory?("#{root}/#{dir}/#{thing}")}.compact!
+    images = Dir.entries("#{root}/#{dir}").map{|thing| thing unless thing.match(/^\./) || File.directory?("#{root}/#{dir}/#{thing}")}.compact! rescue []
     images = images.map {|name| name if name.match(/(^.+\.jpg|^.+\.png)/)}.compact.sort
     array_string="['#{images.join('\',\'')}']"
-    haml :layout_1, :locals => {:images => images, :dir => dir, :array_string => array_string} 
+    haml :gallery, :locals => {:images => images, :dir => dir, :array_string => array_string} 
   end
 end
