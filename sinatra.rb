f8ef8fa349a904d7abc8812377a8ca5a54b7ad59
Matt Mills <sunrisetimes@gmail.com>
2012-04-23 20:47:39 -0500

    thumbnails code mostly done

--- sinatra.rb
+++ sinatra.rb
@@ -5,30 +5,43 @@ require 'sinatra'
 require 'haml'
 class Gallery < Sinatra::Base
   root="/home/matt/public_html/images"
+  
   get '/' do
     images = Dir.entries(root).map{|thing| thing unless thing.match(/^\./) || File.directory?("#{root}/#{thing}")}.compact! rescue []
     images = images.map {|name| name if name.match(/(^.+\.jpg|^.+\.png)/)}.compact.sort
     haml :index, :locals => {:images => images, :dir => "", :array_string => ""}
   end
   
-  get '/:dir' do
+  get '/:dir/?:image?' do
     path = "#{root}/#{params[:dir]}"
+      
+    check_thumbnails path
     
-    #check for thumbnails and make them if they don't exist; morgrify is a part of imagemagick
-    unless File.directory? "#{path}/thumbnails"
-      `mkdir #{path}/thumbnails && cd #{path} && mogrify -format png -path thumbnails -auto-orient -thumbnail 100x100 '*.jpg'`
+    images = get_images(path)
+    
+    thumbnails = get_images("#{path}/thumbnails")
+    
+    if params[:image]
+      image_index = params[:image]
+    else
+      image_index = 0
     end
-    
-    #get the images in a usable array
-    Dir.chdir path
-    images = Dir["*.jpg", "*.png"]
-    
-    #do the same for thumbnails
-    Dir.chdir "#{path}/thumbnails"
-    thumbnails = Dir["*.jpg", "*.png"]
     
     array_string="['#{images.join('\',\'')}']"
     
-    haml :gallery, :locals => {:images => images, :dir => dir, :array_string => array_string} 
+    haml :gallery, :locals => {:images => images, :image_index => image_index, :dir => dir, :array_string => array_string} 
+  end
+
+  def get_images(path)
+   Dir.chdir "#{path}"
+   return Dir["*.jpg", "*.png"]
+  end
+  
+  def check_thumbnails(path)
+    #check for thumbnails and make them if they don't exist; morgrify is a part of imagemagick; 
+    # see http://www.imagemagick.org/Usage/thumbnails/ for details etc
+    unless File.directory? "#{path}/thumbnails"
+      `mkdir #{path}/thumbnails && cd #{path} && mogrify -format png -path thumbnails -auto-orient -thumbnail 100x100 '*.jpg'`
+    end
   end
 end
