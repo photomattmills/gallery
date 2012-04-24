22ae99edf8059500e0d08ec33805fe582db7186c
Matt Mills <sunrisetimes@gmail.com>
2012-04-23 22:08:28 -0500

    add thumbnails to layout

--- sinatra.rb
+++ sinatra.rb
@@ -12,7 +12,7 @@ class Gallery < Sinatra::Base
     haml :index, :locals => {:images => images, :dir => "", :array_string => ""}
   end
   
-  get '/:dir/:image?' do
+  get '/:dir/?:image' do
     img_path = "#{root}/#{params[:dir]}"
     
     check_thumbnails img_path
