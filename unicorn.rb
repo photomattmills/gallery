1a2075e1a7a412554b4a43daa87f780c7c7937a5
Matt Mills <sunrisetimes@gmail.com>
2012-11-09 15:37:52 -0600

    changes to unicorn.rb to make deploys work

--- unicorn.rb
+++ unicorn.rb
@@ -9,11 +9,11 @@ timeout 30
 
 # Specify path to socket unicorn listens to, 
 # we will use this in our nginx.conf later
-listen "#{@dir}tmp/sockets/unicorn.sock", :backlog => 64
+listen "/tmp/unicorn/sockets/unicorn.sock", :backlog => 64
 
 # Set process id path
-pid "#{@dir}tmp/pids/unicorn.pid"
+pid "/var/run/unicorn/pids/unicorn.pid"
 
 # Set log file paths
-stderr_path "#{@dir}log/unicorn.stderr.log"
-stdout_path "#{@dir}log/unicorn.stdout.log"
+stderr_path "/var/log/unicorn/unicorn.stderr.log"
+stdout_path "/var/log/unicorn/unicorn.stdout.log"
